8bd1d725517d179ecf0f2838a7ecd833c577b75b
Tharani8103 <tharani.mca2024@adhiyamaan.in>
2025-12-25 13:13:05 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,3 +3,14 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 };
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
